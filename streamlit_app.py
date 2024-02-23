8f28d400135522ff6cde13237d807116149d0cfd
op3ntrap <op3ntrap@gmail.com>
2024-02-23 17:16:54 +0530

    Add performance banner image and update presentation URL

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,4 @@ import streamlit.components.v1 as components
 
 st.title("ONDC Index Presentation URL")
 components.iframe("https://docs.google.com/presentation/d/e/2PACX-1vRst3OatJS2Ajr7kPxQIcs19JWf7SN7bclcuK8h4pOvcSBpZeo7p-kGErc6aX67uFnomVetgHk-s6hx/embed?start=false&loop=false&delayms=3000",width=1440, height=839, scrolling=False)
+st.image("images/perf_banner.svg")  
