9e3c051a2baf6268f4715f825fd50611eee9825c
op3ntrap <op3ntrap@gmail.com>
2024-02-23 17:22:46 +0530

    Add presentation URL and performance metrics

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,10 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 st.title("ONDC Index Presentation URL")
-components.iframe("https://docs.google.com/presentation/d/e/2PACX-1vRst3OatJS2Ajr7kPxQIcs19JWf7SN7bclcuK8h4pOvcSBpZeo7p-kGErc6aX67uFnomVetgHk-s6hx/embed?start=false&loop=false&delayms=3000",width=1440, height=839, scrolling=False)
 st.image("images/perf_banner.svg")  
+components.iframe("https://docs.google.com/presentation/d/e/2PACX-1vRst3OatJS2Ajr7kPxQIcs19JWf7SN7bclcuK8h4pOvcSBpZeo7p-kGErc6aX67uFnomVetgHk-s6hx/embed?start=false&loop=false&delayms=3000",width=1440, height=839, scrolling=False)
+st.text("Time to complete 10,000 searches ~ 90s")
+st.text("Roughly 6667 Searches per minute on a single instance")
+
+
+st.image("images/10000-requests.gif", width=1280)
