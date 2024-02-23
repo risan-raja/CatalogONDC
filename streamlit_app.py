f2d83be5ede2f5d78f64b4c4e9b55d89f69b883c
op3ntrap <op3ntrap@gmail.com>
2024-02-23 17:12:52 +0530

    Add ONDC Index Presentation URL iframe

---
+++ streamlit_app.py
@@ -0,0 +1,6 @@
+# <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vRst3OatJS2Ajr7kPxQIcs19JWf7SN7bclcuK8h4pOvcSBpZeo7p-kGErc6aX67uFnomVetgHk-s6hx/embed?start=false&loop=false&delayms=3000" frameborder="0" width="1440" height="839" allowfullscreen="true" mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
+import streamlit as st
+import streamlit.components.v1 as components
+
+st.title("ONDC Index Presentation URL")
+components.iframe("https://docs.google.com/presentation/d/e/2PACX-1vRst3OatJS2Ajr7kPxQIcs19JWf7SN7bclcuK8h4pOvcSBpZeo7p-kGErc6aX67uFnomVetgHk-s6hx/embed?start=false&loop=false&delayms=3000",width=1440, height=839, scrolling=False)
